9e278675746d1599cf51353cfc27c8a58a3c6c18
ducaoya <ducaoya@qq.com>
2021-07-24 22:02:38 +0800

    适配多种屏幕

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,51 @@
-import React from 'react';
-import { Doing } from './component/Doing';
-import { Todo } from './component/Todo';
-import {Done} from "./component/Done";
-import { observer } from 'mobx-react'
-import {AddTask} from "./component/AddTask";
-import { Row, Col } from 'antd';
+import React from "react";
+import { Doing } from "./component/Doing";
+import { Todo } from "./component/Todo";
+import { Done } from "./component/Done";
+import { observer } from "mobx-react";
+import { AddTask } from "./component/AddTask";
+import { Row, Col } from "antd";
 
 @observer
-export default class App extends React.Component<any, any>{
-    render() {
-        return (
-            <Row style={{ justifyContent: "center" }}>
+export default class App extends React.Component<any, any> {
+  render() {
+    return (
+      <Row style={{ justifyContent: "center" }}>
         <Col span={18}>
           <Row>
             <AddTask></AddTask>
           </Row>
-          <Row style={{ width: "100%", justifyContent: "space-between" }}>
-            <Col span={7}>
+          <Row
+            style={{ width: "100%", justifyContent: "space-between" }}
+            gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
+          >
+            <Col
+              xs={{ span: 24 }}
+              sm={{ span: 24 }}
+              md={{ span: 24 }}
+              lg={{ span: 8 }}
+            >
               <Todo></Todo>
             </Col>
-            <Col span={8}>
+            <Col
+              xs={{ span: 24 }}
+              sm={{ span: 24 }}
+              md={{ span: 24 }}
+              lg={{ span: 8 }}
+            >
               <Doing></Doing>
             </Col>
-            <Col span={8}>
+            <Col
+              xs={{ span: 24 }}
+              sm={{ span: 24 }}
+              md={{ span: 24 }}
+              lg={{ span: 8 }}
+            >
               <Done></Done>
             </Col>
           </Row>
         </Col>
       </Row>
-        );
-    }
+    );
+  }
 }
--- src/component/AddTask.tsx
+++ src/component/AddTask.tsx
@@ -1,80 +1,100 @@
 import React from "react";
-import store from "../store/Store"
-import { observer } from 'mobx-react'
+import store from "../store/Store";
+import { observer } from "mobx-react";
 import "antd/dist/antd.css";
 import { Button, Col, Input, Row } from "antd";
 
 interface State {
-    value: string,
-    name: string
+  value: string;
+  name: string;
 }
 
 @observer
-export class AddTask extends React.Component<any, State>{
-    // @ts-ignore
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: '',
-            name: ''
-        }
-    }
-    // @ts-ignore
-    submit(event) {
-        store.change(this.state.value, this.state.name)
-        this.setState({
-            value: "",
-            name: ""
-        })
-        event.preventDefault()
-    }
+export class AddTask extends React.Component<any, State> {
+  // @ts-ignore
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      name: "",
+    };
+  }
+  // @ts-ignore
+  submit(event) {
+    store.change(this.state.value, this.state.name);
+    this.setState({
+      value: "",
+      name: "",
+    });
+    event.preventDefault();
+  }
 
-    // @ts-ignore
-    changeEvent(event) {
-        this.setState({
-            value: event.target.value
-        })
-    }
+  // @ts-ignore
+  changeEvent(event) {
+    this.setState({
+      value: event.target.value,
+    });
+  }
 
-    // @ts-ignore
-    changeName(event) {
-        this.setState({
-            name: event.target.value
-        })
-    }
+  // @ts-ignore
+  changeName(event) {
+    this.setState({
+      name: event.target.value,
+    });
+  }
 
-    render() {
-        return (
-            <Row style={{ width: "100%", justifyContent: "center" }}>
-                <Col span={3} style={{ marginRight: '10px' }}>
-                    <Input
-                        placeholder="姓名"
-                        allowClear
-                        size="large"
-                        value={this.state.name}
-                        onChange={(event) => this.changeName(event)}
-                    />
-                </Col>
-                <Col span={12} style={{ marginRight: '10px' }}>
-                    <Input
-                        placeholder="添加待办事项"
-                        allowClear
-                        size="large"
-                        value={this.state.value}
-                        onChange={(event) => this.changeEvent(event)}
-                        onPressEnter={(event) => this.submit(event)}
-                    />
-                </Col>
-                <Col span={1} >
-                    <Button
-                        type="primary"
-                        size='large'
-                        onClick={(event) => this.submit(event)}
-                    >
-                        添加
-                    </Button>
-                </Col>
-            </Row>
-        )
-    }
+  render() {
+    return (
+      <Row
+        style={{ width: "100%", justifyContent: "center" }}
+        gutter={[16, 16]}
+      >
+        <Col
+          xs={{ span: 24 }}
+          sm={{ span: 24 }}
+          md={{ span: 14 }}
+          lg={{ span: 3 }}
+        >
+          <Input
+            placeholder="姓名"
+            allowClear
+            size="large"
+            value={this.state.name}
+            onChange={(event) => this.changeName(event)}
+          />
+        </Col>
+        <Col
+          xs={{ span: 24 }}
+          sm={{ span: 24 }}
+          md={{ span: 14 }}
+          lg={{ span: 8 }}
+        >
+          <Input
+            placeholder="添加待办事项"
+            allowClear
+            size="large"
+            value={this.state.value}
+            onChange={(event) => this.changeEvent(event)}
+            onPressEnter={(event) => this.submit(event)}
+          />
+        </Col>
+        <Col
+          xs={{ span: 8 }}
+          sm={{ span: 8 }}
+          md={{ span: 14 }}
+          lg={{ span: 3 }}
+          style={{ display: "flex", justifyContent: "center" }}
+        >
+          <Button
+            type="primary"
+            size="large"
+            style={{ width: "100%" }}
+            onClick={(event) => this.submit(event)}
+          >
+            添加
+          </Button>
+        </Col>
+      </Row>
+    );
+  }
 }
